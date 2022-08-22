27fa3933d447cb320786f2b574f688070b5bf048
Ladrillo <gabriel@cabrejas.net>
2022-08-22 14:40:52 +0200

    upgrade deps

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './components/App'
 import './styles/reset.css'
 import './styles/styles.css'
 
-render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <>
     <h1>Todo App</h1>
     <App />
-  </React.StrictMode>
-  , document.getElementById('root')
+  </>
 )

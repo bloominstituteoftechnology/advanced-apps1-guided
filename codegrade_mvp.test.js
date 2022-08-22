daf4ce8170f6832f9e9ceadf076e41a91b20ddef
Ladrillo <gabriel@cabrejas.net>
2022-08-22 19:07:55 +0200

    committing to "lecture"

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -43,7 +43,8 @@ test('<Todo /> component renders completion of todos correctly', () => {
     toggleStatus={Function.prototype}
   />)
   expect(screen.queryByText('Alex ✔️')).toBeInTheDocument()
-  screen.queryByText('lady gaga')
+  screen.queryByText('lady gaga') // this is null
+  screen.getByText('lady gaga')
 })
 test.todo('<Todo /> Clicking on todo fires the toggleStatus handler')
 test.todo('<App /> Todos can be completed by the user')

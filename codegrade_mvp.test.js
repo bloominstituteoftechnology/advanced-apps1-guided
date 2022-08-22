3a6985da6c2f2b64227230c2fedc26e397c9d15f
Ladrillo <gabriel@cabrejas.net>
2022-08-22 15:54:44 +0200

    setup

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -1,22 +1,23 @@
+// mock server imports
 import server from './backend/mock-server'
-import React from 'react'
-import App from './frontend/components/App'
-import { render, screen, fireEvent } from '@testing-library/react'
 import { resetTodos } from './backend/helpers'
-import '@testing-library/jest-dom/extend-expect'
+// we are going use JSX so we need React
+import React from 'react'
+// imports from the testing libraries
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect' // jest matchers for the DOM
+// components to test
+import App from './frontend/components/App'
+import Todo from './frontend/components/Todo'
 
-// not needed for module project, START
 beforeAll(() => { server.listen() })
 afterAll(() => { server.close() })
 afterEach(() => { server.resetHandlers() })
 beforeEach(() => { resetTodos() })
-// not needed for module project, END
 
-test('App is NOT a class-based component', async () => {
-  render(<App />)
-  await screen.findByText('laundry')
-  expect(
-    App.prototype &&
-    App.prototype.isReactComponent
-  ).not.toBeTruthy()
-})
+test.todo('<App /> renders learners synchronously')
+test.todo('<App /> renders "laundry" asynchronously')
+test.todo('<Todo /> component renders completion of todos correctly')
+test.todo('<Todo /> Clicking on todo fires the toggleStatus handler')
+test.todo('<App /> Todos can be completed by the user')
+test.todo('<App /> Can submit a new todo, and shows up uncompleted')
